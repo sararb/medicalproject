9f65ae861d0f7d02bacb9898dc621975a36306d1
Sara <sara@MBP-de-Sara.lan>
2017-05-23 14:34:34 +0200

    ajout des commentaires

--- text_interpreter_f/construct_tree_randomforest.py
+++ text_interpreter_f/construct_tree_randomforest.py
@@ -1,8 +1,10 @@
 import pickle
 
 
+
 def load_obj(name):
     """
+    to load existing model already learned
     :param name: the path of the rf classifier
     :return:
     """
@@ -10,30 +12,53 @@ def load_obj(name):
         return pickle.load(f)
 
 
-def build_infos(rf, instance, feature_names):
+def build_infos(name_clf, instance, feature_names):
     """
 
-    :param rf:
-    :param instance:
-    :param feature_names:
+    :param name_clf: the path of the rf classfier
+    :param instance: if the id of the patient
+    :param feature_names: the vocabulary of the corpus
     :return:
     """
+
+    # load the model
+    rf = load_obj(name_clf)
+
+    # get the trees of the Random forest
     n_tree = rf.n_estimators
 
     # get the infos for each decision tree of the rf model :
+
+    # a dict to save the path that lead to the prediction given by each tree
+    # ##### the key is the id of the tree and the value is the list of the nodes activated
     phrases = {}
+
+    # a dict where the key is the id of a tree and the value is a list of flags "right / left" related to each node
     flag = {}
+
+    # key is the id of tee and the value is a list of the non activated nodes
     missing_words = {}
+
+    # key is id of the tree and the value is an integer related to the predicted class
     classes = {}
     print('Rules used to predict sample %s: ' % instance)
+
+
+
+
     for i in range(n_tree):
         phrases[i] = []
         flag[i] = []
+        # ght the tree i
         estimator = rf.estimators_[i]
+        # get all the nodes of tree i
         feature = estimator.tree_.feature
+        # get the values of the split decision
         threshold = estimator.tree_.threshold
+        # get the index of the activated nodes
         node_indicator = estimator.decision_path(instance)
         activated_node_index = node_indicator.nonzero()[1]
+        # get the predicted class by tree i
         classes[i] = estimator.predict(instance)[0]
 
         for node_id in activated_node_index:
@@ -43,6 +68,7 @@ def build_infos(rf, instance, feature_names):
             else:
                 phrases[i].append(feature_names[feature[node_id]])
                 flag[i].append('right')
+
         missing_words[i] = set(feature_names[feature]).difference(feature_names[feature[activated_node_index]])
 
     return phrases, flag, classes, missing_words
--- text_interpreter_f/learning_rf.py
+++ text_interpreter_f/learning_rf.py
@@ -24,11 +24,26 @@ def learning_(rf, text_train, y, path_clf):
     # the the bag of word representation :
     vectorizer = TfidfVectorizer(max_df=0.8, min_df=3)
     X_train = vectorizer.fit_transform(text_train)
-    features_names = vectorizer.get_feature_names()
-    feature_names = np.asarray(features_names)
+    features_names = np.asarray(vectorizer.get_feature_names())
+    # TODO : add Hyperopt/gridsearch for Hyper-parameter selection
+    # TODO : get the f1-score : from Hyperopt object
+
+    # from sklearn.model_selection import cross_val_predict
+    # predicted = cross_val_predict(rf_best, X_train, y, cv=5)
+    # from sklearn.metrics import confusion_matrix
+    # confusion_matrix(predicted, y)
+    # compute the f1 score
+
+    # apply our best model to all the dataframe
     rf.fit(X_train, y)
+
+
+
     directory = os.getcwd()
+    # save the learned best  model
     save_obj(rf, os.path.join(directory, path_clf))
 
-    return rf, X_train, feature_names, vectorizer
+    # TODO : save the evaluation metrics
+
+    return rf, X_train, features_names
 
