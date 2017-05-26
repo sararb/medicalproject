19d2b2e5a2e6ef5f40e4821f8f15d1c3c38b465c
pereira <pereira.sebastien.74@gmail.com>
2017-05-26 17:07:33 +0200

    MySQL application connection

--- interface/Main.py
+++ interface/Main.py
@@ -5,6 +5,7 @@ import tkinter as tk
 from tkinter.filedialog import *
 import pickle
 import pandas as pd
+import MySQLdb
 
 # Main application which allows us to navigate through windows
 class Application(tk.Tk):
@@ -52,13 +53,53 @@ class Page1(tk.Frame):
         button2 = tk.Button(self, text="Importer des fichiers patients", command=self.importer_des_données, )
         button2.pack(side="top")
 
+        button3 = tk.Button(self, text="Afficher les fichiers patients", command=self.see_database_content, )
+        button3.pack(side="top")
+
+        button4 = tk.Button(self, text="Ajouter des données à la BDD", command=self.write_in_database, )
+        button4.pack(side="top")
+
     def importer_des_données(self):
+        #Getting path to data we want to load
         filepath = askopenfilename(title="Import de dossiers patient",
                                    filetypes=[('csv files', '.csv'), ('all files', '.*')])
-        new_data = pd.read_csv(filepath, sep=';', encoding='utf_8')
-        new_data.to_pickle('/home/sebastien/PycharmProjects/PFE/medicalproject/interface/new_data.csv')
-        print(new_data.head(10))
 
+        # Putting data into a dataframe
+        new_data = pd.read_csv(filepath, sep=';', encoding='utf_8')
+
+        # Apparently pickling will be use only to store models
+        #new_data.to_pickle('/home/sebastien/PycharmProjects/PFE/medicalproject/interface/new_data.csv')
+
+        #print(new_data.head(10))
+        print("Size")
+        print(new_data.size)
+        print("\n")
+        print("Info")
+        print(new_data.info())
+        print("\n")
+        #print("Data")
+        #for i in range(0,new_data.size-1)
+        #print (new_data)
+        #print(new_data.get_value(1, 'patient_id'))
+
+    def see_database_content(self):
+        connection = MySQLdb.connect(host="localhost", user="root", passwd="J455Dkje4815D14DkekD", db="medical_database")
+
+        c = connection.cursor()
+        c.execute("SELECT * FROM patient_database")
+        rows = c.fetchall()
+        for eachRow in rows:
+            print(eachRow)
+
+    def write_in_database(self):
+        connection = MySQLdb.connect(host="localhost", user="root", passwd="Kaoutar08Ftouhi", db="medical_database")
+
+        c = connection.cursor()
+        id_patient = 5
+        review = 'Dossier médical'
+        classe = 0
+        c.execute("INSERT INTO patient_database (patient_id, review, class) VALUES (%s, %s, %s)",
+                  (id_patient, review, classe))
 
 # Windows which allows the user to train new models
 class Page2(tk.Frame):
